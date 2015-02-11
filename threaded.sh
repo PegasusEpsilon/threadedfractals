bff8b6e18b7e917f0ba6122e8c8bd184ce00d3e8
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-11 08:11:57 -0600

    moved the make above the rm. don't delete if you can't compile.

--- threaded.sh
+++ threaded.sh
@@ -2,8 +2,8 @@
 
 source config.sh
 
-rm threaded.map threaded.msaa threaded.rgb
 make palette.bin threaded render resample pngify || exit
+rm threaded.map threaded.msaa threaded.rgb
 mkfifo threaded.map; time ./threaded 4 10 $MSAA_REAL $MSAA_IMAG $CENTER_REAL $CENTER_IMAG $RADIUS_REAL $RADIUS_IMAG $THETA threaded.map &
 mkfifo threaded.msaa; ./render -l threaded.map palette.bin 0 $DIVIDER threaded.msaa &
 mkfifo threaded.rgb; ./resample threaded.msaa $SIZE_REAL $MSAA threaded.rgb &
