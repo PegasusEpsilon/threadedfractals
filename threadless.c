f42b3ad19953b8460b274a2297262a686100e79c
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-15 07:21:40 -0600

    update threadless.c to match threaded.c (minus threads)

--- threadless.c
+++ threadless.c
@@ -9,59 +9,54 @@
 #include "sample.h"
 #include "types.h"
 
-#define new(x) malloc(sizeof(x))
 #define _hot  __attribute__((hot ))	/* called often */
 #define _cold __attribute__((cold))	/* called rarely */
 #define unlikely(x) __builtin_expect(x, 0)
 
+long double *buffer;
+FILE *output_file;
+unsigned long long next_line = 0;
 struct pixel max;
-unsigned long long render = 0, write;
 
-void display (void) {
-	printf("%llu/%llu (%llu%%)\r", render, max.imag, 100 * render / max.imag);
+static inline _hot void display (void) {
+	printf(
+		"%llu/%llu (%02.02f%%)\x1b[K\r",
+		next_line, max.imag, next_line / (float)max.imag * 100
+	);
 	fflush(stdout);
 }
 
 FILE *output_file;
-static inline _hot unsigned long long output (long double *data) {
-	fwrite(data, sizeof(long double), max.real, output_file);
-	free(data);
-	fflush(output_file);
-	unsigned long long next = (render == max.imag ? (unsigned long long)-1 : render++);
-	return next;
+static inline _hot void output (void) {
+	fwrite(buffer, sizeof(long double), max.real, output_file);
+	display();
+	next_line++;
 }
 
 long double theta;
 long double complex pixelsize;
 struct region viewport;
-static inline _hot long double *iterate_line (
-	const unsigned long long line
-) {
+static inline _hot void iterate_line () {
 	struct coordinates_4d coordinates = { .z = 0 + 0 * I };
-	long double *buffer = malloc(max.real * sizeof(long double));
-	struct pixel this = { .imag = line };
+	struct pixel this = { .imag = next_line };
 
 	for (this.real = 0; this.real < max.real; this.real++) {
 		coordinates.c = pixel2vector(&this, &pixelsize, &viewport, &theta);
 		buffer[this.real] = sample(&coordinates);
 	}
-	return buffer;
 }
 
-static inline void *thread (void *ptr) {
-	unsigned long long thread = (unsigned long long)ptr;
-	while ((unsigned long long)-1 != (
-		thread = output(iterate_line(thread))
-	)) { display(); }
-	return NULL;
+static void thread (void) {
+	do {
+		iterate_line();
+		output();
+	} while (next_line != max.imag);
 }
 
 __attribute__((noreturn))
 void _cold usage (char *myself) {
-	puts("Threaded Mandelbrot sampler\n");
-	printf("Usage: %s THREADS LIMIT WIDTH HEIGHT CEN_REAL CEN_IMAG RAD_REAL RAD_IMAG THETA OUTFILE\n\n", myself);
-	puts("	THREADS	how many threads to spawn");
-	puts("	LIMIT	max pending writes before threads block");
+	puts("Unthreaded Mandelbrot sampler\n");
+	printf("Usage: %s WIDTH HEIGHT CEN_REAL CEN_IMAG RAD_REAL RAD_IMAG THETA OUTFILE\n\n", myself);
 	puts("	WIDTH	number of horizontal samples");
 	puts("	HEIGHT	number of vertical samples");
 	puts("	center coordinates (CEN_REAL, CEN_IMAG)");
@@ -82,14 +77,26 @@ int main (int argc, char **argv) {
 
 	max.real = atoi(argv[1]);
 	max.imag = atoi(argv[2]);
-	viewport.center = strtold(argv[3], NULL) - strtold(argv[4], NULL) * I;
-	viewport.radius = strtold(argv[5], NULL) + strtold(argv[6], NULL) * I;
+	/* force GCC -ffast-math to be sensible */
+	long double tmp1 = strtold(argv[3], NULL);
+	long double tmp2 = strtold(argv[4], NULL);
+	viewport.center = tmp1 - tmp2 * I;
+	tmp1 = strtold(argv[5], NULL);
+	tmp2 = strtold(argv[6], NULL);
+	viewport.radius = tmp1 + tmp2 * I;
 	theta = strtold(argv[7], NULL);
 	output_file = fopen(argv[8], "w");
 
+	/* cache some math */
 	pixelsize = calculate_pixelsize(&max, &viewport);
 
-	thread((void *)render);
+	/* allocate output buffer */
+	buffer = calloc(max.real, sizeof(long double));
+	thread();
+	display();
+	puts("\n\nDone!");
+
+	fclose(output_file);
 
 	return 0;
 }
