fb4692ae3c8158c2e75ee54aaad4a5d2b86fead6
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-21 05:57:21 -0600

    rewired tiled.sh, other .sh will follow eventually

--- tiled.sh
+++ tiled.sh
@@ -2,13 +2,14 @@
 
 source A0.sh
 
+cleanup () { rm tiled.map tiled.msaa tiled.rgb; exit; }
+
+mkfifo tiled.rgb tiled.msaa tiled.map
+
+trap cleanup 1 2 4 5 6 7 8 11 13 14 15
+
 make palette.bin threaded render resample tiler pngify || exit
-rm threaded.map threaded.msaa threaded.rgb
-mkfifo threaded.rgb
-./tiler threaded.rgb $SIZE_REAL $SIZE_IMAG 20 20 threaded.png ./pngify {infile} {width} {height} {outfile} &
-mkfifo threaded.msaa
-./resample threaded.msaa $SIZE_REAL $MSAA threaded.rgb &
-mkfifo threaded.map
-./render -l threaded.map palette.bin 0 $DIVIDER threaded.msaa &
-time ./threaded $THREADS $MSAA_REAL $MSAA_IMAG $CENTER_REAL $CENTER_IMAG $RADIUS_REAL $RADIUS_IMAG $THETA threaded.map
-rm threaded.map threaded.msaa threaded.rgb
+./tiler tiled.rgb $SIZE_REAL $SIZE_IMAG 20 20 threaded.png ./pngify {infile} {width} {height} {outfile} &
+./resample tiled.msaa $SIZE_REAL $MSAA tiled.rgb &
+./render -l tiled.map palette.bin 0 $DIVIDER tiled.msaa &
+time ./threaded $THREADS $MSAA_REAL $MSAA_IMAG $CENTER_REAL $CENTER_IMAG $RADIUS_REAL $RADIUS_IMAG $THETA tiled.map
