16c27ad5552ce8a49653c366b3c8cc1fd96734f0
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-21 14:59:24 -0600

    added a preemptive cleanup() to shell scripts

--- config.sh
+++ config.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
-MSAA=16
+MSAA=1
 SIZE_REAL=$((1*1920))
 SIZE_IMAG=$((1*1080))
 CENTER_REAL=-0.75
 CENTER_IMAG=0
 RADIUS_REAL=2
 THETA=0
-DIVIDER=1
 SAMPLER=crosstrap.so
 SAMPLER_ARGS="0.5 1 0"
+DIVIDER=1.5
+FLATTEN=
