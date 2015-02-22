d09136ab9b079678ad80fe5c3458f55e94c08c8d
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-22 12:51:22 -0600

    tweak the cleanup hooks

--- julia.sh
+++ julia.sh
@@ -16,13 +16,14 @@ RADIUS_IMAG=$(echo "scale=40;$RADIUS_REAL*$SIZE_IMAG/$SIZE_REAL"|bc|sed -e 's/0*
 MSAA_REAL=$(($SIZE_REAL*$MSAA))
 MSAA_IMAG=$(($SIZE_IMAG*$MSAA))
 
-cleanup () { rm julia.map julia.msaa julia.rgb; exit; }
-rm julia.map julia.msaa julia.rgb
+cleanup () { rm julia.map julia.msaa julia.rgb; }
+cleanup
 mkfifo julia.rgb julia.msaa julia.map
-trap cleanup 1 2 3 4 5 6 7 8 11 13 14 15
+trap "cleanup; exit" 1 2 3 4 5 6 7 8 11 13 14 15
 
 make palette.bin threaded render resample pngify $SAMPLER || exit
 ./pngify julia.rgb $SIZE_REAL $SIZE_IMAG julia.png &
 ./resample julia.msaa $SIZE_REAL $MSAA julia.rgb &
 ./render $FLATTEN julia.map palette.bin 0 $DIVIDER julia.msaa &
 time ./threaded $THREADS $MSAA_REAL $MSAA_IMAG $CENTER_REAL $CENTER_IMAG $RADIUS_REAL $RADIUS_IMAG $THETA julia.map $SAMPLER $SAMPLER_ARGS
+cleanup
--- threadless.sh
+++ threadless.sh
@@ -6,13 +6,14 @@ RADIUS_IMAG=$(echo "scale=40;$RADIUS_REAL*$SIZE_IMAG/$SIZE_REAL"|bc|sed -e 's/0*
 MSAA_REAL=$(($SIZE_REAL*$MSAA))
 MSAA_IMAG=$(($SIZE_IMAG*$MSAA))
 
-cleanup () { rm threadless.map threadless.msaa threadless.rgb; exit; }
-rm threadless.map threadless.msaa threadless.rgb
+cleanup () { rm threadless.map threadless.msaa threadless.rgb; }
+cleanup
 mkfifo threadless.map threadless.msaa threadless.rgb
-trap cleanup 1 2 3 4 5 6 7 8 11 13 14 15
+trap "cleanup; exit" 1 2 3 4 5 6 7 8 11 13 14 15
 
 make palette.bin threadless render resample pngify $SAMPLER || exit
 time ./threadless 4 10 $MSAA_REAL $MSAA_IMAG $CENTER_REAL $CENTER_IMAG $RADIUS_REAL $RADIUS_IMAG $THETA threadless.map &
 ./render $FLATTEN threadless.map palette.bin 0 $DIVIDER threadless.msaa &
 ./resample threadless.msaa $SIZE_REAL $MSAA threadless.rgb &
 ./pngify threadless.rgb $SIZE_REAL $SIZE_IMAG threadless.png $SAMPLER $SAMPLER_ARGS
+cleanup
