26ec13dac8696e2824d20c0476b5564be4caf581
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-02-25 02:48:23 -0600

    added other coordinates, changed sampler to use them

--- config.sh
+++ config.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 THREADS=$(cat /proc/cpuinfo | grep processor | wc -l)
-MSAA=1
+MSAA=16
 SIZE_REAL=1920
 SIZE_IMAG=1080
-CENTER_REAL=0
+
+# full set
+CENTER_REAL=-0.75
 CENTER_IMAG=0
-SAMPLER="scale 2.2 translate -0.75 0 mandelbrot renormalized escape_count 16"
-DIVIDER=10
+RADIUS_REAL=2.2
+
+# nautilus
+CENTER_REAL=-0.7766729
+CENTER_IMAG=-0.13661091
+RADIUS_REAL=0.00016
+
+# "bird of paradise" stress test
+#CENTER_REAL=0.3750001200618655
+#CENTER_IMAG=0.2166393884377127
+#RADIUS_REAL=0.000000000002
+
+SAMPLER="scale $RADIUS_REAL translate $CENTER_REAL $CENTER_IMAG mandelbrot renormalized escape_count 16"
+DIVIDER=1
 FLATTEN=-l
 PALETTE=blueglow.txt
 RENDERER=threaded
