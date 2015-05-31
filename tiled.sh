35bae0c27f5342ba70d43600670b4ebe68fd1cb4
Pegasus Epsilon <pegasus@pimpninjas.org>
2015-05-31 02:44:57 -0500

    Fixed path to blueglow.txt

--- tiled.sh
+++ tiled.sh
@@ -17,7 +17,7 @@ mkfifo tiled.rgb tiled.msaa tiled.map
 trap cleanup 1 2 4 5 6 7 8 11 13 14 15
 
 make palette threaded modules render resample tiler pngify || exit
-./palette blueglow.txt palette.bin
+./palette palettes/blueglow.txt palette.bin
 ./tiler tiled.rgb $SIZE_REAL $SIZE_IMAG 20 20 threaded.png ./pngify {infile} {width} {height} {outfile} &
 ./resample tiled.msaa $SIZE_REAL $MSAA tiled.rgb &
 ./render $FLATTEN tiled.map palette.bin 0 $DIVIDER tiled.msaa &
