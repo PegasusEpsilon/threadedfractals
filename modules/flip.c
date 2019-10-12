4eca7b4b5997dc0a8196edcfd87f288fc4def232
Pegasus Epsilon <pegasus@pimpninjas.org>
2019-10-12 04:34:41 -0500

    scale module doesn't need to know about pi, add flip and mirror because easy enough, and somebody might need them?

--- modules/flip.c
+++ modules/flip.c
@@ -3,8 +3,7 @@
 
 __attribute__((cold, noreturn, always_inline)) static inline
 void usage (char *myself) {
-	printf("Usage: ... %s SCALE SAMPLER ARGS\n\n", myself);
-	puts("	SCALE	smaller = zoom more");
+	printf("Usage: ... %s SAMPLER ARGS\n\n", myself);
 	puts("	SAMPLER	shared object file containing complex sampler function");
 	puts("	ARGS	any extra arguments required by the complex sampler");
 	puts("\nReport bugs to pegasus@pimpninjas.org");
@@ -16,20 +15,19 @@ void usage (char *myself) {
 #include "loader.h" 	/* get_sampler() */
 #include "sampler.h"	/* sampler() */
 
-static FLOAT radius;
 static sampler(real_sample);
 
 __attribute__((cold))
 void init (char **argv) {
 	int argc = 0;
 	while (argv[++argc]);
-	if (3 > argc) usage(argv[0]);
-	radius = strtold(argv[1], NULL);
-	real_sample = (sampler())get_sampler(&argv[2]);
+	if (2 > argc) usage(argv[0]);
+	real_sample = (sampler())get_sampler(&argv[1]);
 }
 
 __attribute__((hot, pure))
 FLOAT sample (complex FLOAT *point) {
-	*point *= radius;
+	// I feel like there should be a simpler operation here...
+	*point = CREAL(*point) - CIMAG(*point) * I;
 	return real_sample(point);
 }
