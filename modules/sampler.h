4a236db36f4eb54294d79a05e5bcb72fe77c97e4
Pegasus Epsilon <pegasus@pimpninjas.org>
2020-11-22 07:15:44 -0500

    Removed ancient cruft

--- modules/sampler.h
+++ modules/sampler.h
@@ -4,7 +4,6 @@
 #ifdef COMPLEX
 	#define sampler(x) FLOAT (*x) (complex FLOAT *const, complex FLOAT *const)
 #else
-	typedef int stfu_gcc;
 	#define sampler(x) FLOAT (*x) (FLOAT complex *const)
 #endif
 
